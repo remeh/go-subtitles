a9778123927cf7f9a20f44bd60e1b1b229c8e942
remeh <me@remy.io>
2014-12-29 22:37:47 +0100

    Computes a score based on files words.

--- src/analyzer/subtitle.go
+++ src/analyzer/subtitle.go
@@ -4,10 +4,10 @@
 
 package analyzer
 
-import ()
+import "strings"
 
 // Analyzes the filename to return a analyze result.
-func CompareFilenameSubtitleName(fileURI string, subtitleURI string) {
+func CompareFilenameSubtitleName(fileURI string, subtitleURI string) float32 {
 	filename := GetFilename(fileURI)
 	sName := GetFilename(subtitleURI)
 
@@ -22,5 +22,13 @@ func CompareFilenameSubtitleName(fileURI string, subtitleURI string) {
 	// The idea is to split by spaces and look how much of the "words"
 	// of the subtitles filename we can find in the video filename
 
-	// TODO Should be applied on MovieReleaseName and SubFileName
+	words := strings.Split(sName, " ")
+	found := 0
+	for _, word := range words {
+		if strings.Contains(filename, word) {
+			found++
+		}
+	}
+
+	return float32(found) / float32(len(words))
 }
